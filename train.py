47197a47c24041380190f868a97f1534a04e5e06
kkamaGod <2859040695@qq.com>
2026-03-01 15:38:18 +0800

    重命名 main.py -> predict.py，添加 train.py 和 requirements.txt，更新 misc.xml

---
+++ train.py
@@ -0,0 +1,10 @@
+import torch
+import transformers
+import pandas as pd
+import sklearn
+import flask
+import matplotlib
+
+print("环境测试成功！")
+print("Torch版本:", torch.__version__)
+print("Transformers版本:", transformers.__version__)
